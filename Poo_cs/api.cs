f75bbdc689b922dba4481a62d3c750b607299f31
vinici0sal-cdg <viniciusgarcialoreto@gmail.com>
2024-06-29 22:22:29 -0300

    Initial commit

---
+++ Poo_cs/api.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace Course
+{
+    class Tst{
+        static void Main(string[] args){
+
+            object api = ("https://api.tomticket.com/criar_chamado/{token}/{identificador_cliente}");
+            Dictionary<string, string> info = new Dictionary<string, string>();
+
+            info["bom dia"] = "bom dia";  css
+        }
+    }
+}
