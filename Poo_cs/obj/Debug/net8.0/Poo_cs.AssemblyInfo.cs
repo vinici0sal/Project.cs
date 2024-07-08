03735530aa8b36d48e45ee3585464a87b717fce6
vinici0sal-cdg <viniciusgarcialoreto@gmail.com>
2024-07-08 10:45:09 -0300

    Commit

--- Poo_cs/obj/Debug/net8.0/Poo_cs.AssemblyInfo.cs
+++ Poo_cs/obj/Debug/net8.0/Poo_cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Poo_cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5939a1e6c88ef87f0e3ae4c4173d670a677ca4de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3819c377508b770839c2e5be8dcde6ab8987e5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Poo_cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Poo_cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
